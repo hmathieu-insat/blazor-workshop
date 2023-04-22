8fda304fc77a256d1c3e435ec78d87aae4040d1f
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-04-22 10:49:32 +0200

    💄 Update PizzaConfigureDialog

---
+++ save-points/00-get-started/BlazingPizza.Client/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazingPizza.Client.Pages;
+
+public partial class Index
+{
+    private List<PizzaSpecial> specials;
+    private Pizza configuringPizza;
+    private bool showingConfigureDialog;
+    private Order order = new Order();
+
+    private void ShowConfigurePizzaDialog(PizzaSpecial special)
+    {
+        configuringPizza = new Pizza()
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>(),
+        };
+
+        showingConfigureDialog = true;
+    }
+
+    protected async override Task OnInitializedAsync()
+    {
+        specials = await HttpClient.GetFromJsonAsync<List<PizzaSpecial>>("specials", BlazingPizza.OrderContext.Default.ListPizzaSpecial);
+    }
+
+
+}
---
+++ save-points/00-get-started/BlazingPizza.Client/Shared/ConfigurePizzaDialog.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+
+namespace BlazingPizza.Client.Shared;
+public partial class ConfigurePizzaDialog
+{
+    [Parameter] public Pizza Pizza { get; set; }
+    [Parameter] public bool Show { get; set; }
+    [Parameter] public EventCallback OnCancel { get; set; }
+    [Parameter] public EventCallback OnConfirm { get; set; }
+
+    private List<Topping> _toppings = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        _toppings = await Http.GetFromJsonAsync<List<Topping>>("toppings");
+    }
+
+    private void ToppingSelected(ChangeEventArgs e)
+    {
+        if (int.TryParse((string)e.Value, out var index) && index >= 0)
+            AddTopping(_toppings[index]);
+    }
+
+    private void AddTopping(Topping topping)
+    {
+        if (!Pizza.Toppings.Exists(pt => pt.ToppingId == topping.Id))
+            Pizza.Toppings.Add(new PizzaTopping { Topping = topping });
+    }
+
+    private void RemoveTopping(Topping topping)
+    {
+        Pizza.Toppings.RemoveAll(pt => pt.Topping.Id == topping.Id);
+    }
+}
