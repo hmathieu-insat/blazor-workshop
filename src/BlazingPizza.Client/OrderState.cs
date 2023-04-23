f7d735f8aab1ffd9e6a10f9de436be2b119f593b
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-04-23 03:25:13 +0200

    ♻️ Update AppState

---
+++ src/BlazingPizza.Client/OrderState.cs
@@ -0,0 +1,48 @@
+﻿namespace BlazingPizza.Client;
+
+public class OrderState
+{
+    public bool ShowingConfigureDialog { get; private set; }
+
+    public Pizza ConfiguringPizza { get; private set; }
+
+    public Order Order { get; private set; } = new();
+
+    public void ShowConfigurePizzaDialog(PizzaSpecial pizzaSpecial)
+    {
+        ConfiguringPizza = new Pizza()
+        {
+            Special = pizzaSpecial,
+            SpecialId = pizzaSpecial.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>(),
+        };
+        ShowingConfigureDialog = true;
+    }
+
+    public void CancelConfigurePizzaDialog()
+    {
+        ConfiguringPizza = null;
+        ShowingConfigureDialog = false;
+    }
+
+    public void ConfirmConfigurePizzaDialog()
+    {
+        if (ConfiguringPizza is not null)
+        {
+            Order.Pizzas.Add(ConfiguringPizza);
+            ConfiguringPizza = null;
+        }
+        ShowingConfigureDialog = false;
+    }
+
+    public void RemoveConfiguredPizza(Pizza pizza)
+    {
+        Order.Pizzas.Remove(pizza);
+    }
+
+    public void ResetOrder()
+    {
+        Order = new();
+    }
+}
