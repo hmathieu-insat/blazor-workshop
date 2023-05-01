6f4f47ca94e3540defd136194c140387e8cae12b
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-05-01 10:49:25 +0200

    🧱 Add overposting guard

--- src/BlazingPizza.Client/Pages/Checkout.razor.cs
+++ src/BlazingPizza.Client/Pages/Checkout.razor.cs
@@ -4,14 +4,24 @@ namespace BlazingPizza.Client.Pages;
 
 public partial class Checkout
 {
+    private bool _isSubmitting = false;
+
     /// <summary>
     /// Places the order asynchronous.
     /// </summary>
     async Task PlaceOrderAsync()
     {
-        var response = await Http.PostAsJsonAsync("orders", OrderState.Order);
-        var newOrderId = await response.Content.ReadFromJsonAsync<int>();
-        OrderState.ResetOrder();
-        NavigationManager.NavigateTo($"/myorders/{newOrderId}");
+        _isSubmitting = true;
+        try
+        {
+            var response = await Http.PostAsJsonAsync("orders", OrderState.Order);
+            var newOrderId = await response.Content.ReadFromJsonAsync<int>();
+            OrderState.ResetOrder();
+            NavigationManager.NavigateTo($"/myorders/{newOrderId}");
+        }
+        finally
+        {
+            _isSubmitting = false;
+        }
     }
 }
